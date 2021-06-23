a9b07b5d04bf43d8d77b09feb239a9fc1f7a4c35
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2021-06-23 16:27:09 -0400

    change state matdrawer

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -12,7 +12,7 @@ export class LayoutComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit(): void {
-    this.matDrawer.close();
+    this.matDrawer.open();
   }
   changeStateSidenav(state: boolean) {
     this.matDrawer.toggle();
