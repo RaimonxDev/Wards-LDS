f54b22d089259884d5b708610339417341809fb6
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2021-07-08 17:37:44 -0400

    workin in details

--- src/app/admin/modules/minuta/components/details-minuta/details-minuta.component.ts
+++ src/app/admin/modules/minuta/components/details-minuta/details-minuta.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Minuta } from '../../models/minuta.models';
+import { Minuta, tipoMinutas } from '../../models/minuta.models';
 import { MinutaService } from '../../services/minuta.service';
 import { tap } from 'rxjs/operators';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertService } from 'src/app/ui/alert/services/alert.service';
+import { AuthService } from 'src/app/core/auth/services/auth.service';
 
 @Component({
   selector: 'app-details-minuta',
@@ -11,11 +14,61 @@ import { tap } from 'rxjs/operators';
 })
 export class DetailsMinutaComponent implements OnInit {
   minuta$!: Observable<Minuta>;
-  constructor(private _minutaServices: MinutaService) {}
+  tipoMinuta!: Observable<tipoMinutas[]>;
+
+  formMinuta!: FormGroup;
+
+  editForm: boolean = false;
+
+  // Getters and Setters
+  get discursantesArr() {
+    return this.formMinuta.get('discursantes') as FormArray;
+  }
+  get relevosArr() {
+    return this.formMinuta.get('relevos') as FormArray;
+  }
+  get sostenimientosArr() {
+    return this.formMinuta.get('sostenimientos') as FormArray;
+  }
+  constructor(
+    private _minutaServices: MinutaService,
+    private _fb: FormBuilder,
+    private _alert: AlertService,
+    private _authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.initFormMinuta();
+
     this.minuta$ = this._minutaServices.minuta$.pipe(
       tap((data) => console.log(data))
     );
+  }
+
+  initFormMinuta() {
+    this.formMinuta = this._fb.group({
+      tipos_de_minuta: ['', [Validators.required]],
+      completa: [false],
+      barrio: [this._authService.barrioID],
+      preside: ['', [Validators.required]],
+      dirige: ['', [Validators.required]],
+      fecha: ['', [Validators.required]],
+      preludioMusica: [''],
+      reconocimientos: [''],
+      anuncios: [''],
+      primer_himno: [''],
+      primera_oracion: [''],
+      relevos: this._fb.array([]),
+      sostenimientos: this._fb.array([]),
+      himno_sacramental: [''],
+      discursantes: this._fb.array([]),
+      ultimo_himno: [''],
+      ultima_oracion: [''],
+    });
+  }
+
+  editMode() {
+    this.editForm = !this.editForm;
+    console.log(this.editForm);
   }
 }
