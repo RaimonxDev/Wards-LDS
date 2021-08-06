569f00cf0fcd4e76f144cc58008c1c61449f110d
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-08-06 10:32:37 -0400

    add Date formats and delete another imports

---
+++ src/app/admin/modules/minuta/utils/setDates.ts
@@ -0,0 +1,18 @@
+import { addDays } from 'date-fns';
+
+export function addOneDay(fecha: Date) {
+  const oneDayMore = addDays(new Date(fecha), 1);
+  return oneDayMore;
+}
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
