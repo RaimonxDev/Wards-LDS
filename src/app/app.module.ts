f800e57251d87ad5627dc31c180acb943189f1fc
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-08-10 09:23:29 -0400

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { AlertModule } from './ui/alert/alert.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { InterceptorsTokenService } from './core/interceptors/token.service';
 import { DateFnsModule } from 'ngx-date-fns';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -27,6 +29,12 @@ import { DateFnsModule } from 'ngx-date-fns';
     NgProgressModule,
     NgProgressHttpModule,
     AlertModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     // DateFnsModule.forRoot(),
   ],
   providers: [
